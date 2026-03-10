bf5d086706c65adb1c4441c61e3e7e55fcda3de5
HUMANCODERMJ <humancodermj@gmail.com>
2026-03-10 18:31:12 +0530

    implemented counter using lus script

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI, Request, status
-from redis_client import setValue
+from redis_client import r
 
 
 app = FastAPI()
 
 @app.get("/")
 async def root(request: Request):
-    await setValue()
     return {"message": status.HTTP_200_OK}
 
 @app.get("/proxy")
 async def proxy(request: Request):
+    script = """
+    local key = KEYS[1]
+    local count = redis.call('INCR', key)
+    if count == 1 then
+        redis.call('EXPIRE', key, 60)
+    end
+    return count
+    """
+    r.incr
+    count = await r.eval(script, 1, "gateway:minute_counter")
     response = dummy_backend()
+    response["request_count"] = count
     return response
 
 def dummy_backend():
--- redis_client.py
+++ redis_client.py
@@ -1,6 +1,3 @@
 from redis.asyncio import Redis
 
 r = Redis(host='localhost', port=6379, decode_responses=True)
-
-async def setValue():
-    await r.set(1, "Hello")
